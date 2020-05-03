5e760c24aec4ef7c4b15c78d2ff6ee4829e6bd4a
Anshul Singhvi <asinghvi17@simons-rock.edu>
2020-05-03 14:20:49 +0530

    stufff

--- figures.jl
+++ figures.jl
@@ -14,7 +14,9 @@ AbstractPlotting.set_theme!(
         ygridwidth = 0.4,
         xticksize = -3,
         yticksize = -3,
-        titlegap  = 5.
+        titlegap  = 5,
+        xticklabelpad = 5,
+        yticklabelpad = 5,
     )
 )
 
@@ -47,4 +49,30 @@ lines!(ax, cycloid(1, .65))
 # plot the straight-line path and hte
 lines!(ax, [Point2f0(0), Point2f0(0, .65), Point2f0(1, .65)]; linestyle = [4, 8], linewidth = .7)
 lines!(ax, [Point2f0(0), Point2f0(1, .65)]; linestyle = [4, 8], linewidth = .7)
+
 save("brachistochrone.pdf", scene)
+
+
+
+function runbenchmarks(scene::Scene)
+
+    png = @benchmark save("benchmark.png", $(scene)) teardown=(rm("benchmark.png"))
+    printstyled("PNG benchmark:\n"; bold = true, color = :green)
+    show(stdout, MIME("text/plain"), png)
+    println()
+
+    svg = @benchmark save("benchmark.svg", $(scene)) teardown=(rm("benchmark.svg"))
+    printstyled("SVG benchmark:\n"; bold = true, color = :green)
+    show(stdout, MIME("text/plain"), svg)
+    println()
+
+    pdf = @benchmark save("benchmark.pdf", $(scene)) teardown=(rm("benchmark.pdf"))
+    printstyled("PDF benchmark:\n"; bold = true, color = :green)
+    show(stdout, MIME("text/plain"), pdf)
+    println()
+
+    cbuf = @benchmark AbstractPlotting.colorbuffer($(CairoMakie.CairoScreen(scene)))
+    printstyled("Colorbuffer benchmark:\n"; bold = true, color = :green)
+    show(stdout, MIME("text/plain"), cbuf)
+    println()
+end
